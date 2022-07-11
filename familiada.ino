75a2e42c27d9477ae736a5ef2d28e2bc87889d73
bagietka <black_market@onet.eu>
2022-07-11 19:57:41 +0200

    Color of LED and button changed; no changes to logic

--- familiada.ino
+++ familiada.ino
@@ -1,18 +1,20 @@
 #define BTN_RESET 12
 #define BTN_BLUE 6
 #define LED_BLUE 7
-#define BTN_YELLOW 8
-#define LED_YELLOW 9
+#define BTN_GREEN 8
+#define LED_GREEN 9
 
 int currentColor = -1;
 
 void setup() {
   pinMode(BTN_RESET, INPUT_PULLUP);
   pinMode(BTN_BLUE, INPUT_PULLUP);
-  pinMode(BTN_YELLOW, INPUT_PULLUP);
+  pinMode(BTN_GREEN, INPUT_PULLUP);
 
   pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+      digitalWrite(LED_GREEN, HIGH);
+          digitalWrite(LED_BLUE, HIGH);
 }
 
 void loop() {
@@ -22,16 +24,16 @@ void loop() {
     digitalWrite(LED_BLUE, HIGH);
   }
 
-  if( digitalRead(BTN_YELLOW) == LOW and currentColor == -1 )
+  if( digitalRead(BTN_GREEN) == LOW and currentColor == -1 )
   {
-    currentColor = BTN_YELLOW;
-    digitalWrite(LED_YELLOW, HIGH);
+    currentColor = BTN_GREEN;
+    digitalWrite(LED_GREEN, HIGH);
   }
 
   if( digitalRead(BTN_RESET) == LOW )
   {
     currentColor = -1;
-    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_GREEN, LOW);
     digitalWrite(LED_BLUE, LOW);
   }
 
